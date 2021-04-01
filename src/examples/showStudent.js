d050f00763c9b046bc61e4d7934c5725ca394638
Pavel Solovev <pashius2010@gmail.com>
2021-04-01 08:20:23 +0400

    showStudent update

--- src/examples/showStudent.js
+++ src/examples/showStudent.js
@@ -1,3 +1,14 @@
 export const showStudent = (ssn) => {
   const student = db.find(ssn);
+  if (student !== null) {
+    document.querySelector(`${elementId}`).innerHTML = `
+      ${student.ssn},
+      ${student.firstname},
+      ${student.lastname}
+    `;
+  } else {
+    throw new Error("Student not found!");
+  }
 };
+
+showStudent("777-77-7777");
